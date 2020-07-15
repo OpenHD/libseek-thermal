c310c4dd1f1bb817b1b43cd8ae4ef09773eeb7c3
Stephen Oliver <steve@infincia.com>
2020-07-15 13:22:33 -0400

    Add package script

---
+++ package.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+PLATFORM=$1
+DISTRO=$2
+
+
+if [[ "${PLATFORM}" == "pi" ]]; then
+    OS="raspbian"
+    ARCH="arm"
+    PACKAGE_ARCH="armhf"
+fi
+
+apt -y install cmake libusb-1.0-0-dev libopencv-dev libboost-program-options-dev
+
+PACKAGE_NAME=libseek-thermal
+
+TMPDIR=/tmp/${PACKAGE_NAME}-installdir
+
+rm -rf ${TMPDIR}/*
+
+mkdir -p ${TMPDIR}/usr/local/bin || exit 1
+mkdir -p ${TMPDIR}/usr/local/lib || exit 1
+mkdir -p ${TMPDIR}/usr/local/include/seek || exit 1
+mkdir -p build
+
+pushd build
+cmake ../
+make clean || exit 1
+make || exit 1
+cp -a examples/seek_viewer ${TMPDIR}/usr/local/bin/ || exit 1
+cp -a src/libseek.so ${TMPDIR}/usr/local/lib/ || exit 1
+cp -a src/libseek_static.a ${TMPDIR}/usr/local/lib/ || exit 1
+
+cp -a ../src/SeekCam.h ${TMPDIR}/usr/local/include/seek/ || exit 1
+cp -a ../src/SeekDevice.h ${TMPDIR}/usr/local/include/seek/ || exit 1
+cp -a ../src/seek.h ${TMPDIR}/usr/local/include/seek/ || exit 1
+cp -a ../src/SeekLogging.h ${TMPDIR}/usr/local/include/seek/ || exit 1
+cp -a ../src/SeekThermal.h ${TMPDIR}/usr/local/include/seek/ || exit 1
+cp -a ../src/SeekThermalPro.h ${TMPDIR}/usr/local/include/seek/ || exit 1
+
+popd
+
+VERSION=$(git describe)
+
+rm ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
+
+fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TMPDIR} \
+  -p ${PACKAGE_NAME}_VERSION_ARCH.deb \
+  -d "libboost-program-options-dev" \
+  -d "libopencv-video-dev" \
+  -d "libusb-1.0-0 >= 1.0" || exit 1
+
+
+#
+# Only push to cloudsmith for tags. If you don't want something to be pushed to the repo, 
+# don't create a tag. You can build packages and test them locally without tagging.
+#
+git describe --exact-match HEAD > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    echo "Pushing package to OpenHD repository"
+    cloudsmith push deb openhd/openhd-2-0/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb
+else
+    echo "Not a tagged release, skipping push to OpenHD repository"
+fi
